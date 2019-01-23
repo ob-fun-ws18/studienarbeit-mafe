19e2439b1a05a298837ea57ec1cac3a689f1e649
Felix Maurer <felix@felix-maurer.de>
2019-01-23 19:00:47 +0100

    Document more functions in Server

--- src/Server.hs
+++ src/Server.hs
@@ -61,7 +61,13 @@ acceptConnections sock chan = do
     forkIO (startConn conn chan)
     acceptConnections sock chan
 
-mainServer :: Chan Event -> Users -> Channels -> IO ()
+-- |The main server waits for events that the clients generate. It also keeps track
+-- of the connected users and the channels. It modifies the states and sends replys
+-- to the clients.
+mainServer :: Chan Event -- ^The channels to receive events from the clients
+           -> Users      -- ^The currently connected users with their handles
+           -> Channels   -- ^The currently available channels with their users
+           -> IO ()
 mainServer chan users channels = do
     event <- readChan chan
     print event
@@ -110,22 +116,39 @@ handleEvent users channels (ClientMsg user@(FullUser nick _ _) msg@(IsOn nicks))
     return (users, channels)
 handleEvent users channels _ = return (users, channels)
 
-sendToUser :: Users -> String -> String -> IO ()
+-- |This function sends a message to a specified user.
+sendToUser :: Users  -- ^The map of users to look up the correct handle
+           -> String -- ^The nickname of the user to send to
+           -> String -- ^The message to send
+           -> IO ()
 sendToUser users nick = hPutStrLn (users ! nick)
 
-sendToAllUsers :: Users -> [String] -> String -> IO ()
+-- |This function sends the same message to many users.
+sendToAllUsers :: Users    -- ^The map of users to look up the correct handles
+               -> [String] -- ^The nicknames of the users to send to
+               -> String   -- ^The message to send
+               -> IO ()
 sendToAllUsers _ [] _ = return ()
 sendToAllUsers users receivers msg = do
     sendToUser users (head receivers) msg
     sendToAllUsers users (tail receivers) msg
 
-startConn :: (Socket, SockAddr) -> Chan Event -> IO ()
+-- |This function sets up the connection to a client and starts the loop that handles
+-- the further communication.
+startConn :: (Socket, SockAddr) -- ^The socket information for the client
+          -> Chan Event         -- ^The channel to communictate with the main server
+          -> IO ()
 startConn (sock, _) chan = do
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
     loopConn hdl chan NoUser
 
-loopConn :: Handle -> Chan Event -> User -> IO ()
+-- |This function handles the communication from the client to the server. It reads
+-- the messages, parses them, and acts accordingly.
+loopConn :: Handle     -- ^The handle to receive data from the client
+         -> Chan Event -- ^The channel to communicate with the main server
+         -> User       -- ^The data of the user that is handled by this connection
+         -> IO ()
 loopConn hdl chan user = do
     eof <- hIsEOF hdl
     if eof then
